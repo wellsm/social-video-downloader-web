7ba82d26be24782b056ab7f07a364c03c99ece2c
Well <wellington.martins@uello.com.br>
2024-08-26 19:59:51 -0300

    fix: log base url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,6 +2,8 @@
 import { USER } from "@/app/contexts/auth";
 import axios from "axios";
 
+console.log(import.meta.env.VITE_API_BASE_URL);
+
 export const client = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
